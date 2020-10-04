68e128076ffe092f61506adf2de7b1c02b7ec79b
Cheikh Tidiane Khoule <ctkhoule@gmail.com>
2020-10-04 13:58:52 +0000

    Ajout index et controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route pour appeler index
+Route::get('/boutique', [ProductController::class, 'index']);
